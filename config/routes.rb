9eab9edcc49618c9b5488416abce1780f2d1eca1
loinguyen2050 <loinguyengd@gmail.com>
2017-05-08 20:05:17 +0700

    first_commit_and_design_homepage

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root "static_pages#index"
+  devise_for :users
+
+  resources :users
+end
---
+++ db/migrate/20170508113739_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :avatar
+      t.date :day_of_birth
+      t.string :gender
+      t.string :phone_number
+      t.boolean :is_admin
+      t.boolean :is_publisher
+      t.string :job_description
+
+      t.timestamps
+    end
+  end
+end
